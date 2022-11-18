8eb7bd5ade2114ed0a86051e377b3d78bea94223
Ganiyat Yakub <yakubganiyat96@gmail.com>
2022-11-18 06:06:17 +0100

    writing test

--- src/components/Show.test.tsx
+++ src/components/Show.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
 import Show from "./Show";
 import GoogleMap from "./GoogleMap";
+import axios from "axios";
 
 describe("show component tests", () => {
   test("testing", () => {
